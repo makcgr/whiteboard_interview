389268c6298da39e83208e56f1f54fb4fdc079b2
mgr <max.grachev@gmail.com>
2025-07-25 01:39:02 +0300

    Longest substring without repetitions

---
+++ longest substring without repetition/Program.cs
@@ -0,0 +1,24 @@
+﻿var str = "abrkaabcdefghijjxxx";
+Console.WriteLine($"Answer: {GetLongestSubstringWORepetitions(str)}");
+
+static int GetLongestSubstringWORepetitions(string input)
+{
+	if (input == null || input.Length == 0)
+		return 0;
+
+	var arr = input.ToCharArray();	
+	var head = 0;
+	var tail = 0;
+	var maxLen = 0;
+	var hashSet = new HashSet<char>();
+	while (head < arr.Length) {
+		if (hashSet.Contains(arr[head])) {
+			maxLen = Math.Max(maxLen, head-tail);
+			hashSet.Clear();
+			tail = head;
+		}
+		hashSet.Add(arr[head]);
+		head++;			
+	}
+	return Math.Max(maxLen, head-tail);
+}
