1c9cd809a0cd3cde35868e39d9f54a54f3c6e3ea
mgr <max.grachev@gmail.com>
2025-07-25 03:21:20 +0300

    Longest common prefix

---
+++ longest common prefix/Program.cs
@@ -0,0 +1,19 @@
+﻿var arr = new [] { "flower", "flow", "flight" };
+Console.WriteLine(LongestCommonPrefix(arr));
+
+static string LongestCommonPrefix(string[] arr) {
+	if (arr == null || arr.Length == 0) return string.Empty;
+	if(arr.Length==1) return arr[0];
+
+	int currentLen = 0;
+	for (int i=0; i<arr[0].Length; i++) {
+		for (int j=0; j<arr.Length; j++) {
+			if (arr[j].Length < (i+1) || arr[j][i] != arr[0][i])
+			{
+				return arr[0].Substring(0,currentLen);
+			}
+		}
+		currentLen++;
+	}
+	return arr[0].Substring(0,currentLen);
+} 
