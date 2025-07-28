b59dbfa7d6734186fc9c0902e96b6f2ce7ca98ee
mgr <max.grachev@gmail.com>
2025-07-28 14:23:55 +0300

    3 sum update (make more concise)

--- 3sum/Program.cs
+++ 3sum/Program.cs
@@ -1,47 +1,37 @@
 ﻿
+int [] nums = [-1, 0, 1, 2, -1, -4];
 
-IList<IList<int>> ThreeSum(int[] nums) 
+var res = ThreeSum(nums);
+foreach(var tuple in res)
+    Console.WriteLine($"({tuple.Item1}, {tuple.Item2}, {tuple.Item3})");
+
+Console.WriteLine(string.Join(", "), res);
+
+(int, int, int)[] ThreeSum(int[] nums) 
 {
-    if (nums==null || nums.Length<3) throw new ArgumentException(nameof(nums));
-
-    var result = new List<IList<int>>();
-    var htResults = new HashSet<Tuple<int,int,int>>();
+    if (nums.Length < 3) return [];
+    var result = new HashSet<(int,int,int)>();
 
     // Existing numbers between nums[i] and nums[j]
     for(int i=0; i<nums.Length-2; i++)
     {
         int target = 0 - nums[i];
-        var htExistingNumbers = new HashSet<int>();
+        var hashSet = new HashSet<int>();
         
         for(int j=i+1; j<nums.Length; j++)
         {
-            if(htExistingNumbers.Contains(target-nums[j]))
+            if(hashSet.Contains(target - nums[j]))
             {
-                var array = new[]{nums[i], target-nums[j], nums[j]};
-                Array.Sort(array);
-                var resultTuple = new Tuple<int, int, int>(array[0], array[1], array[2]);
-                if (!htResults.Contains(resultTuple))
-                {
-                    htResults.Add(resultTuple);
-                    result.Add(array);
-                }
+                int[] arr = [nums[i], target - nums[j], nums[j]];
+                Array.Sort(arr);
+                result.Add((arr[0], arr[1], arr[2]));
             }
             else
             {
-                htExistingNumbers.Add(nums[j]);
+                hashSet.Add(nums[j]);
             }
         }
     }
-    return result;    
-}
-
-
-int [] nums = new[] {-1,0,1,2,-1,-4};
-
-var result = ThreeSum(nums);
-
-foreach(var arr in result)
-{
-    Console.Write($"{arr[0]} {arr[1]} {arr[2]}\n");
+    return result.ToArray();
 }
 
