7e2666f2bd3a0b72fe12c19f97d6685a2fdd5899
Max Grachev <m.grachev@life-pay.ru>
2025-07-31 03:25:29 +0300

    group anagrams

---
+++ group anagrams/Program.cs
@@ -0,0 +1,22 @@
+﻿var strs = new string[] { "eat", "tea", "tan", "ate", "nat", "bat" };
+
+foreach (var group in GroupAnagrams(strs).ToArray())
+    Console.Write("{" + string.Join(", ", group.ToArray()) + "}");
+Console.WriteLine();
+
+List<List<string>> GroupAnagrams(string[] strs)
+{
+    var groups = new Dictionary<string, List<string>>();
+    
+    foreach (string str in strs)
+    {
+        string key = string.Concat(str.OrderBy(c => c));
+        
+        if (!groups.ContainsKey(key))
+            groups[key] = new List<string>();
+            
+        groups[key].Add(str);
+    }
+    
+    return groups.Values.Where(list => list.Count>1).ToList();
+}
