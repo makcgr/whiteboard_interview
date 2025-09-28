105eed8ac28c2b062ea65071e79e91263078d9f9
mgr <max.grachev@gmail.com>
2025-09-28 22:02:19 +0300

    Base structures and algorithms (linked list, BFS)

---
+++ base structures/bfs.py
@@ -0,0 +1,46 @@
+from collections import deque
+
+# 1. define Node class
+# 2. define bfs function (accepting root Node parameter)
+# 3. In bfs(root):
+#    define queue (FIFO storage) 
+#    define visited dictionary (hash set)
+#    define result accumulating variable 
+#    
+#    add root node to queue
+#    while queue contains elements:
+#       pop node from queue
+#        process element
+#        if node has children:
+#            add all children to queue
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def bfs(root):
+    queue = [ root ]
+    visited = set()
+    res = 0
+    while len(queue) > 0:
+        node = queue.pop()
+        if node not in visited: 
+            res += node.value
+            visited.add(node)
+
+            if node.left is not None:
+                queue.append(node.left)
+            if node.right is not None:
+                queue.append(node.right)
+    return res
+
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(5)
+res = bfs(root)
+print(res)
---
+++ base structures/linked_list.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+#   None  a->b->c->d->
+#   |     |
+#   prev  cur
+
+# why it works: 
+#   cur and prev, this is the node
+#   - temp to store the next
+#   - reverse cur 
+#   - update prev (to cur)
+#   - update cur  (to saved next)
+
+def reverse(head): 
+    cur = head
+    prev = None
+    while cur is not None:
+        temp = cur.next
+        cur.next = prev
+        prev = cur
+        cur = temp
+
+
+def print_list(node):
+    while node is not None:
+        print(node.value, "->", end="")
+        node = node.next
+    print()
+
+
+node = Node(1)
+node2 = node.next = Node(2)
+node3 = node.next.next = Node(3)
+print_list(node)
+reverse(node)
+print_list(node3)
+
