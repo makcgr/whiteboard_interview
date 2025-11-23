e7f6366eb8e5146d8f0c86a4da874bcd206162bf
mgr <max.grachev@gmail.com>
2025-11-23 03:07:14 +0300

    merge overlapping arrays

---
+++ merge overlapping intervals/program.py
@@ -0,0 +1,22 @@
+# why it works:
+#   - first we need to sort intervals to place them in order of "start"
+#   - we initiate result with first interval from list
+#   - then we simply iterate over the rest intervals 
+#          and if current interval overlaps (start < previous interval's end), 
+#              then we try to update the resulting (previous) interval 
+#              otherwise we add new interval to resulting array
+
+def mergeIntervals(intervals: list[list[int]]) -> list[int]:
+    intervals.sort(key=lambda i: i[0])
+    res = [intervals[0]]
+    for i in intervals[1:]:
+        if i[0] <= res[-1][1]:
+            res[-1][1] = max(res[-1][1], i[1])
+        else:
+            res.append(i)
+
+    return res
+
+input = [[12,15], [4,10], [2,5]]
+res = mergeIntervals(input)
+print(res)
