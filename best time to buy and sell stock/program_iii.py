44856747c8c771f7bb0c69e2f8b2dbabc68be8b5
mgr <max.grachev@gmail.com>
2025-12-19 00:15:54 +0300

    best time to buy and sell stock iii - add a clarifying comment

--- best time to buy and sell stock/program_iii.py
+++ best time to buy and sell stock/program_iii.py
@@ -2,6 +2,7 @@ class Solution:
     def maxProfit(self, prices: list[int], fee: int) -> int:
         cash = 0            # pocket 1: "cash" (how much money if we are NOT holding stock)
         hold = -prices[0]   # pocket 2: "hold" (how much money if we ARE holding stock) 
+                            # NOTE: why -prices[0]: we BORROWED these money to hold stock
 
         for price in prices[1:]:
             cash = max(cash, hold + price - fee) # “If I want to have NO stock today, what’s my best move?”
@@ -9,7 +10,7 @@ class Solution:
         return cash
 
 
-print(Solution().maxProfit([1,3,2,8,4,9],2)
+print(Solution().maxProfit([1,3,2,8,4,9],2))
 
 
 # why this works:
