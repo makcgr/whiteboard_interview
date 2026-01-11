8cd902250a146de66ad69129a151c0b7517f1e52
mgr <max.grachev@gmail.com>
2026-01-12 02:09:23 +0300

    permutations

--- permutations/program.py
+++ permutations/program.py
@@ -1,9 +1,10 @@
 class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
+    def permute(self, nums: list[int]) -> list[list[int]]:
         result = []
         def backtrack(path):
             if len(path) == len(nums):
-                result.append(path[:])            
+                result.append(path[:])
+                return
             for num in nums:
                 if num in path:
                     continue
@@ -14,3 +15,19 @@ class Solution:
         backtrack([])
         return result
 
+# why it works. let's imagine we have input [1, 2, 3]
+# path is our dynamic "backtracking path". 
+# calling backtrack([]) we go like this:
+#   from [] 
+#   -> [1] -> [1,2] -> [1,2,3]* -> [1,2] -> [1] -> [1,3] -> [1,3,2]* -> [1,3] -> [1] -> [] ->
+#   -> [2] -> [2,1] -> [2,1,3]* -> [2,1] -> [2] -> [2,3] -> [2,3,1]* -> [2,3] -> [2] -> [] -> 
+#   -> [3] -> [3,1] -> [3,1,2]* -> [3,1] -> [3] -> [3,2] -> [3,2,1]* -> [3,2] -> [3] -> [] 
+
+# backtracking approach = always 4 steps in the backtrack() func:
+# 1. base case. our: len(path) == len(nums), add copy of path to result arr
+# 2. choice constraints. our: any num in nums, except the ones already in path
+# 3. make choice. our: append num to path
+#    -- call backtrack() function here
+# 4. undo choice. our: pop num from path
+
+print(Solution().permute([1, 2, 3]))
